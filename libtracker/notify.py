9126101f52d26a38b5ea6d18371e539d2c2710c5
euanmills <euanmills26@gmail.com>
2022-05-06 08:57:56 +0100

    Fix broken users append

--- libtracker/notify.py
+++ libtracker/notify.py
@@ -8,19 +8,23 @@ _USER_CACHE = []
 def send_notification(device: str, config: dict) -> None:
     """
     Send an HTTP POST request to the Telegram messaging API.
+
     :param device: The device name that has returned home.
     :param config: Global configuration object.
     :return: None
     """
     if not _USER_CACHE:
         if users := config.setdefault(CONFIG_TELEGRAM_USERS, None):
-            _USER_CACHE.append([user for user in users])
+            if not isinstance(users, list):
+                users = [users]
+            for user in users:
+                _USER_CACHE.append(user)
         else:
             # Telegram users have not been added. Cancel sending the notification.
             return
 
     if not (token := config.setdefault(CONFIG_TELEGRAM_BOT_TOKEN, None)):
-        # No token has been set. Cancel sending the notifcation.
+        # No token has been set. Cancel sending the notification.
         return
 
     message = f"Device: {device} has just returned home"
